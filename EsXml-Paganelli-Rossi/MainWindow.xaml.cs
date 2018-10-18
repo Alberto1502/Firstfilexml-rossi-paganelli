f6c78c3778529919612fae6cfd6656d0bdcad9a0
TheNotoriousCecco <francescorossi300800@gmail.com>
2018-10-18 09:26:09 +0200

    initial commit gio 18 ott 2018 09:26:09

--- EsXml-Paganelli-Rossi/MainWindow.xaml.cs
+++ EsXml-Paganelli-Rossi/MainWindow.xaml.cs
@@ -12,17 +12,54 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
+using System.IO;
 
 namespace EsXml_Paganelli_Rossi
 {
-    /// <summary>
-    /// Logica di interazione per MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void btn_Crea_Click(object sender, RoutedEventArgs e)
+        {
+            //XDocument xmlDocument = XDocument.Load(@"Z:\Tpi\esercizioLibriXML\esercizioLibriXML_Galbucci_Neri\esercizioLibriXML_Galbucci_Neri\libri.xml");
+            XDocument xmlDoc = XDocument.Parse(File.ReadAllText(@"E:\esercizioLibriXML\esercizioLibriXML_Galbucci_Neri\esercizioLibriXML_Galbucci_Neri\libriSer.xml", System.Text.Encoding.UTF8), LoadOptions.None);
+
+            IEnumerable<string> names = from libri in xmlDoc.Descendants("wiride")
+                                        where libri.Element("codice_scheda").Value == "M-FKB0GR01"
+                                        select libri.Element("titolo").Element("proprio").Value;
+
+            foreach (string nomi in names)
+                MessageBox.Show(nomi);
+
+            // xmlDoc.Save(@"E:\esercizioLibriXML\esercizioLibriXML_Galbucci_Neri\esercizioLibriXML_Galbucci_Neri\libriSer.xml");
+        }
+
+        private void BtnFile_Click(object sender, RoutedEventArgs e)
+        {
+            string text = File.ReadAllText(@"E:\esercizioLibriXML\esercizioLibriXML_Galbucci_Neri\esercizioLibriXML_Galbucci_Neri\libriSer.xml", System.Text.Encoding.UTF8);
+            text = text.Replace("\r", "").Replace("\n", "");
+            File.WriteAllText(@"E:\esercizioLibriXML\esercizioLibriXML_Galbucci_Neri\esercizioLibriXML_Galbucci_Neri\libriSer.xml", text);
+        }
+
+        private void btn_Find_Print1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Find_Print2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Find_Print3_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
