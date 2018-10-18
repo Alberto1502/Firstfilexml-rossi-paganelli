2f69bbf99001627dc230ca5df14e1558b4fa47d3
alberto1502 <albi.paganelli@gmail.com>
2018-10-18 09:26:28 +0200

    Lettura dal file libri.xml

--- EsXml-Paganelli-Rossi/MainWindow.xaml.cs
+++ EsXml-Paganelli-Rossi/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Xml.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,5 +25,24 @@ namespace EsXml_Paganelli_Rossi
         {
             InitializeComponent();
         }
+
+        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            XDocument xmlDocument = XDocument.Load(@"C:\Users\alberto.paganelli\Desktop\PrimoEs\libri.xml");
+            
+            IEnumerable<string> names = from libri in xmlDocument.Descendants("wiride")
+                                        where libri.Element("codice_scheda").Value == "M-FKB0GR01"
+                                        select libri.Element("titolo").Element("proprio").Value;
+
+            foreach (string nomi in names)
+                MessageBox.Show(nomi);
+
+
+            XDocument xmlDoc = new XDocument(new XDeclaration("1.0", "utf-8", "yes"),
+            // xmlDoc.Save(@"E:\esercizioLibriXML\esercizioLibriXML_Galbucci_Neri\esercizioLibriXML_Galbucci_Neri\libriSer.xml");
+        }
+    }
     }
 }
